79359ab011a98e98a2939848fe165d82e598ec3a
Zhu XH <zhuxiaohe95@163.com>
2022-08-10 18:51:24 +0800

    feature (LeetCodeSolutions): 完成共 4 题 力扣第 25、70、75 题，剑指 Offer 24 题

---
+++ LeetCodeSolutions/ConsoleApplicationLeetCodeSolutions/from1to200/Solution70.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleApplicationLeetCodeSolutions.from1to200
+{
+    /// <summary>
+    /// 70. 爬楼梯 | 难度：简单 | 标签：记忆化搜索、数学、动态规划
+    /// 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+    /// 
+    /// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+    /// 
+    /// 示例 1：
+    /// 输入：n = 2
+    /// 输出：2
+    /// 解释：有两种方法可以爬到楼顶。
+    /// 1. 1 阶 + 1 阶
+    /// 2. 2 阶
+    /// 
+    /// 示例 2：
+    /// 输入：n = 3
+    /// 输出：3
+    /// 解释：有三种方法可以爬到楼顶。
+    /// 1. 1 阶 + 1 阶 + 1 阶
+    /// 2. 1 阶 + 2 阶
+    /// 3. 2 阶 + 1 阶
+    /// 
+    /// 提示：
+    /// 1 <= n <= 45
+    /// 
+    /// 来源：力扣（LeetCode）
+    /// 链接：https://leetcode.cn/problems/climbing-stairs
+    /// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    /// </summary>
+    public class Solution70
+    {
+        /// <summary>
+        /// 执行用时：20 ms, 在所有 C# 提交中击败了 80.82% 的用户
+        /// 内存消耗：26 MB, 在所有 C# 提交中击败了 47.44% 的用户
+        /// 通过测试用例：45 / 45
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int ClimbStairs(int n)
+        {
+            if (n == 1) return 1;
+            int[] fib = {1, 1};
+            for (int i = 2; i <= n; i++)
+            {
+                fib[i & 1] = fib[0] + fib[1];
+            }
+
+            return fib[n & 1];
+        }
+    }
+}
---
+++ LeetCodeSolutions/ConsoleApplicationLeetCodeSolutions/offer/SolutionOffer24.cs
@@ -0,0 +1,64 @@
+﻿namespace ConsoleApplicationLeetCodeSolutions.offer
+{
+    /// <summary>
+    /// 剑指 Offer 24. 反转链表 | 难度：简单 | 标签：递归，链表
+    /// 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+    /// 
+    /// 示例:
+    /// 输入: 1->2->3->4->5->NULL
+    /// 输出: 5->4->3->2->1->NULL
+    /// 
+    /// 限制：
+    /// 0 <= 节点个数 <= 5000
+    /// 
+    /// 注意：本题与主站 206 题相同：https://leetcode-cn.com/problems/reverse-linked-list/
+    /// 
+    /// 来源：力扣（LeetCode）
+    /// 链接：https://leetcode.cn/problems/fan-zhuan-lian-biao-lcof
+    /// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    /// </summary>
+    public class SolutionOffer24
+    {
+        /// <summary>
+        /// 执行用时：80 ms, 在所有 C# 提交中击败了 66.89% 的用户
+        /// 内存消耗：37.9 MB, 在所有 C# 提交中击败了 82.12% 的用户
+        /// 通过测试用例：27 / 27
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode dummy = new ListNode(-1);
+            ListNode ptr = head;
+            ListNode next;
+            while (ptr != null)
+            {
+                next = ptr.next;
+                ptr.next = dummy.next;
+                dummy.next = ptr;
+                ptr = next;
+            }
+
+            return dummy.next;
+        }
+
+        /**
+         * Definition for singly-linked list.
+         * public class ListNode {
+         *     public int val;
+         *     public ListNode next;
+         *     public ListNode(int x) { val = x; }
+         * }
+         */
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int x)
+            {
+                val = x;
+            }
+        }
+    }
+}
